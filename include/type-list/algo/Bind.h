72b6dd13aae4b692587b810bc402eb8c51f462fe
Darwin Yuan <darwin_yuan@hotmail.com>
2020-07-16 08:20:32 -0700

    added bind

---
+++ include/type-list/algo/Bind.h
@@ -0,0 +1,146 @@
+//
+// Created by godsme on 7/16/20.
+//
+
+#ifndef TYPE_LIST_BIND_H
+#define TYPE_LIST_BIND_H
+
+#include <type-list/type-list-ns.h>
+#include <type-list/concept/NonEmptyListConcept.h>
+#include <type-list/types/Lambda.h>
+#include <type-list/types/List.h>
+#include <cstddef>
+
+TYPE_LIST_NS_BEGIN
+
+template<template<typename ...> typename C, typename FAKE, typename ACTUAL, typename = void>
+struct Combine {
+    template<typename ... Ts>
+    using type = C<Ts...>;
+};
+
+template<template<typename ...> typename C, typename FAKE, typename ACTUAL>
+class Combine<C, FAKE, ACTUAL,
+        std::enable_if_t<std::is_void_v<typename ACTUAL::Head>>> {
+    template <typename ... Ts>
+    using Next = C<typename FAKE::Head, Ts...>;
+
+public:
+    template<typename ... Ts>
+    using type = typename Combine<Next, typename FAKE::Tail, typename ACTUAL::Tail>::template type<Ts...>;
+};
+
+template<template<typename ...> typename C, typename FAKE, typename ACTUAL>
+class Combine<C, FAKE, ACTUAL,
+        std::enable_if_t<!std::is_void_v<typename ACTUAL::Head>>> {
+    template <typename ... Ts>
+    using Next = C<typename ACTUAL::Head, Ts...>;
+public:
+    template<typename ... Ts>
+    using type = typename Combine<Next, FAKE, typename ACTUAL::Tail>::template type<Ts...>;
+};
+
+namespace detail {
+    template<template<typename ...> typename C, size_t N, typename ARGS>
+    struct DoBind;
+
+    template<template<typename ...> typename C, typename ARGS>
+    struct DoBind<C, 0, ARGS> {
+        template<typename ... Ts>
+        using apply = C<Ts...>;
+    };
+
+    template<template<typename ...> typename C, typename ARGS>
+    struct DoBind<C, 1, ARGS> {
+        template<typename T0, typename ... Ts>
+        using apply = typename Combine<C, TypeList<T0>, ARGS>::template type<Ts...>;
+    };
+
+    template<template<typename ...> typename C, typename ARGS>
+    struct DoBind<C, 2, ARGS> {
+        template<typename T0, typename T1, typename ... Ts>
+        using apply = typename Combine<C, TypeList<T0, T1>, ARGS>::template type<Ts...>;
+    };
+
+    template<template<typename ...> typename C, typename ARGS>
+    struct DoBind<C, 3, ARGS> {
+        template<typename T0, typename T1, typename T2, typename ... Ts>
+        using apply = typename Combine<C, TypeList<T0, T1, T2>, ARGS>::template type<Ts...>;
+    };
+
+    template<template<typename ...> typename C, typename ARGS>
+    struct DoBind<C, 4, ARGS> {
+        template<typename T0, typename T1, typename T2, typename T4, typename ... Ts>
+        using apply = typename Combine<C, TypeList<T0, T1, T2, T4>, ARGS>::template type<Ts...>;
+    };
+}
+
+
+namespace detail {
+    template<size_t I, typename T, typename ALL_ARGS>
+    struct ComposeArgs {
+        using args = typename ComposeArgs<I-1, T, typename ALL_ARGS::template appendType<void>>::args;
+    };
+
+    template<typename T, typename ALL_ARGS>
+    struct ComposeArgs<0, T, ALL_ARGS> {
+        using args = typename ALL_ARGS::template appendType<T>;
+    };
+}
+
+namespace detail {
+    template<typename ALL_ARGS, size_t LAST_INDEX,
+            size_t N, typename ... ARGS>
+    struct CalcArgs {
+        constexpr static size_t actualNum = N;
+        using args = ALL_ARGS;
+    };
+
+    template<typename ALL_ARGS, size_t LAST_INDEX,
+            size_t N, typename INDEX, typename T, typename ... ARGS>
+    class CalcArgs<ALL_ARGS, LAST_INDEX, N, INDEX, T, ARGS...> {
+        using argsSoFar = typename ComposeArgs<INDEX::value - LAST_INDEX, T, ALL_ARGS>::args;
+        using result = CalcArgs<argsSoFar, INDEX::value + 1, N + 1, ARGS...>;
+    public:
+        constexpr static size_t actualNum = result::actualNum;
+        using args = typename result::args;
+    };
+
+    template<typename ALL_ARGS, size_t LAST_INDEX,
+            size_t N, typename ILL_FORM, typename ... ARGS>
+    struct CalcArgs<ALL_ARGS, LAST_INDEX, N, ILL_FORM, ARGS...>;
+}
+
+namespace detail {
+    template<template<typename ...> typename C, typename ... ARGS>
+    class Bind {
+        using result = detail::CalcArgs<TypeList<>, 0, 0, ARGS...>;
+    public:
+        using type = typename detail::DoBind<C, result::actualNum, typename result::args>;
+    };
+}
+
+template<template<typename ...> typename C, typename ... ARGS>
+using Bind = typename detail::Bind<C, ARGS...>::type;
+
+namespace detail {
+    template<size_t N>
+    struct Parameter {
+        constexpr static size_t value = N;
+    };
+}
+
+using _0_ = detail::Parameter<0>;
+using _1_ = detail::Parameter<1>;
+using _2_ = detail::Parameter<2>;
+using _3_ = detail::Parameter<3>;
+using _4_ = detail::Parameter<4>;
+using _5_ = detail::Parameter<5>;
+using _6_ = detail::Parameter<6>;
+using _7_ = detail::Parameter<7>;
+using _8_ = detail::Parameter<8>;
+using _9_ = detail::Parameter<9>;
+
+TYPE_LIST_NS_END
+
+#endif //TYPE_LIST_BIND_H
---
+++ tests/TestBind.cpp
@@ -0,0 +1,97 @@
+//
+// Created by godsme on 7/16/20.
+//
+
+#include <type-list/algo/Bind.h>
+#include <type_traits>
+#include <catch.hpp>
+
+namespace {
+    using namespace TYPE_LIST_NS;
+
+    template<typename ... Ts>
+    struct Foo {};
+
+    template<typename T> struct S;
+    SCENARIO("ComposeArgs") {
+
+        WHEN("compose the 0 parameter") {
+            using type = typename detail::ComposeArgs<0, double, TypeList<>>::args;
+            THEN("should padding nothing") {
+                REQUIRE(std::is_same_v<TypeList<double>, type>);
+            }
+        }
+
+        WHEN("compose the 0 parameter with non-empty list") {
+            using type = typename detail::ComposeArgs<0, double, TypeList<int>>::args;
+            THEN("should padding nothing") {
+                REQUIRE(std::is_same_v<TypeList<int, double>, type>);
+            }
+        }
+
+        WHEN("compose the 3rd parameter") {
+            using type = typename detail::ComposeArgs<2, double, TypeList<>>::args;
+            THEN("should padding 2 void") {
+                REQUIRE(std::is_same_v<TypeList<void, void, double>, type>);
+            }
+        }
+
+        WHEN("compose the 3rd parameter with non-empty") {
+            using type = typename detail::ComposeArgs<2, double, TypeList<int>>::args;
+            THEN("should padding 2 void") {
+                REQUIRE(std::is_same_v<TypeList<int, void, void, double>, type>);
+            }
+        }
+
+    }
+
+    SCENARIO("CalcArgs") {
+
+        GIVEN("bind the 0, 3") {
+            using type = detail::CalcArgs<TypeList<>, 0, 0, _0_, int, _3_, double>;
+            THEN("the number should be 2") {
+                REQUIRE(type::actualNum == 2);
+            }
+            THEN("the result args should be padded 2 void") {
+                REQUIRE(std::is_same_v<type::args, TypeList<int, void, void, double>>);
+            }
+        }
+
+        GIVEN("bind the 1, 3") {
+            using type = detail::CalcArgs<TypeList<>, 0, 0, _1_, int, _3_, double>;
+            THEN("the number should be 2") {
+                REQUIRE(type::actualNum == 2);
+            }
+            THEN("the result args should be padded 2 void") {
+                REQUIRE(std::is_same_v<type::args, TypeList<void, int, void, double>>);
+            }
+        }
+    }
+
+    SCENARIO("Combine") {
+        //using type = typename Bind<Foo, _1_, int, _3_, double>::template type<char, float>;
+        using type = Combine<Foo, TypeList<int, double>, TypeList<void, char, void, long>>;
+        REQUIRE(std::is_same_v<typename type::type<float>, Foo<int, char, double, long, float>>);
+    }
+
+    SCENARIO("DoBind") {
+        using bind = detail::DoBind<Foo, 2, TypeList<void, char, void, long>>;
+        using BindResult = typename bind::template apply<int, double, float>;
+        REQUIRE(std::is_same_v<BindResult, Foo<int, char, double, long, float>>);
+    }
+
+    SCENARIO("Bind") {
+        using Bound = Bind<Foo, _1_, int, _3_, double>;
+        WHEN("apply a bound template directly") {
+            using type = typename Bound::template apply<char, float, long>;
+            THEN("should put the applied types in correct position") {
+                REQUIRE(std::is_same_v<type, Foo<char, int, float, double, long>>);
+            }
+        }
+
+        WHEN("pass it to a type list") {
+            using type = decltype(TypeList<char, float, long>::exportTo<Bound::template apply>());
+            REQUIRE(std::is_same_v<type, Foo<char, int, float, double, long>>);
+        }
+    }
+}

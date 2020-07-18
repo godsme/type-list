404c66b41d3c8cae8270d1967d604894521fd94c
Darwin Yuan <darwin_yuan@hotmail.com>
2020-07-18 02:01:36 -0700

    refactored compose

--- include/type-list/algo/Compose.h
+++ include/type-list/algo/Compose.h
@@ -26,68 +26,68 @@ namespace detail {
 
 namespace detail {
     template<CallableConcept H, size_t SIZE, typename ... OPs>
-    struct DoCompose {
+    struct ComposeTraits {
         template<typename ... INPUT>
         using apply = __TL_call(detail::Compose<OPs...>, __TL_call(H, INPUT...));
     };
 
     template<CallableConcept H, typename ... OPs>
-    struct DoCompose<H, 1, OPs...> {
+    struct ComposeTraits<H, 1, OPs...> {
         template<typename T>
         using apply = __TL_call(detail::Compose<OPs...>, __TL_call(H, T));
     };
 
     template<CallableConcept H, typename ... OPs>
-    struct DoCompose<H, 2, OPs...> {
+    struct ComposeTraits<H, 2, OPs...> {
         template<typename T1, typename T2>
         using apply = __TL_call(detail::Compose<OPs...>, __TL_call(H, T1, T2));
     };
 
     template<CallableConcept H, typename ... OPs>
-    struct DoCompose<H, 3, OPs...> {
+    struct ComposeTraits<H, 3, OPs...> {
         template<typename T1, typename T2, typename T3>
         using apply = __TL_call(detail::Compose<OPs...>, __TL_call(H, T1, T2, T3));
     };
 
     template<CallableConcept H, typename ... OPs>
-    struct DoCompose<H, 4, OPs...> {
+    struct ComposeTraits<H, 4, OPs...> {
         template<typename T1, typename T2, typename T3, typename T4>
         using apply = __TL_call(detail::Compose<OPs...>, __TL_call(H, T1, T2, T3, T4));
     };
 
     template<CallableConcept H, typename ... OPs>
-    struct DoCompose<H, 5, OPs...> {
+    struct ComposeTraits<H, 5, OPs...> {
         template<typename T1, typename T2, typename T3, typename T4, typename T5>
         using apply = __TL_call(detail::Compose<OPs...>, __TL_call(H, T1, T2, T3, T4, T5));
     };
 
     template<CallableConcept H, typename ... OPs>
-    struct DoCompose<H, 6, OPs...> {
+    struct ComposeTraits<H, 6, OPs...> {
         template<typename T1, typename T2, typename T3, typename T4, typename T5, typename T6>
         using apply = __TL_call(detail::Compose<OPs...>, __TL_call(H, T1, T2, T3, T4, T5, T6));
     };
 
     template<CallableConcept H, typename ... OPs>
-    struct DoCompose<H, 7, OPs...> {
+    struct ComposeTraits<H, 7, OPs...> {
         template<typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7>
         using apply = __TL_call(detail::Compose<OPs...>, __TL_call(H, T1, T2, T3, T4, T5, T6, T7));
     };
 
     template<CallableConcept H, typename ... OPs>
-    struct DoCompose<H, 8, OPs...> {
+    struct ComposeTraits<H, 8, OPs...> {
         template<typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7, typename T8>
         using apply = __TL_call(detail::Compose<OPs...>, __TL_call(H, T1, T2, T3, T4, T5, T6, T7, T8));
     };
 
     template<CallableConcept H, typename ... OPs>
-    struct DoCompose<H, 9, OPs...> {
+    struct ComposeTraits<H, 9, OPs...> {
         template<typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7, typename T8, typename T9>
         using apply = __TL_call(detail::Compose<OPs...>, __TL_call(H, T1, T2, T3, T4, T5, T6, T7, T8, T9));
     };
 }
 
 template<CallableConcept H, CallableConcept ... OPs>
-using Compose = detail::DoCompose<H, __DEDUCE_TEMPLATE_ARGS(H::template apply)::value, OPs...>;
+using Compose = detail::ComposeTraits<H, __DEDUCE_TEMPLATE_ARGS(H::template apply)::value, OPs...>;
 
 TYPE_LIST_NS_END
 

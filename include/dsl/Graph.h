affd0d7a08e713cbec1cab94afd8394521f18b6a
Darwin Yuan <darwin_yuan@hotmail.com>
2020-08-13 22:28:24 -0700

    added fork

--- include/dsl/Graph.h
+++ include/dsl/Graph.h
@@ -15,10 +15,9 @@
 
 template <typename ... NODES>
 struct Graph {
-    using nodes = __TL_list(typename NODES::NodeType...);
-    static_assert(nodes::size == __TL_unique(nodes)::size, "duplicated nodes");
+    using Nodes = __TL_list(typename NODES::NodeType...);
+    static_assert(Nodes::size == __TL_unique(Nodes)::size, "duplicated nodes");
 
-    using AllNodes = __TL_unique(__TL_concat(__TL_list(nodes, typename NODES::Decendents...)));
     using SortedNodes = typename NodesSort<NODES...>::sortedNodes;
     static_assert(NodesSort<NODES...>::allDecedentsOfRoot::size == (SortedNodes::size - 1), "unreachable nodes exists");
 };
